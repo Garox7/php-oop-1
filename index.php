c8e962fa0c23179a04235f3212b617791482e10a
Garox7 <110902920+Garox7@users.noreply.github.com>
2023-01-09 17:52:11 +0100

    Set constructor and method of the object

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
+<?php
+class Movie {
+    public $title;
+    public $overview;
+    public $vote;
+    public $lang;
+    public $genres = [];
+    public $casts = [];
+
+    function __construct($_title, $_vote, $_lang) {
+        $this->title = $_title;
+        $this->vote = strval($_vote);
+        $this->lang = $_lang; 
+    }
+
+    public function setGenres($_genArray) {
+        for ($i = 0; $i < count($_genArray); $i++) {
+            $this->genres[] = $_genArray[$i];
+        }
+    }
+}
+
+$provaFilm = new Movie('Harry Potter', 5, 'IT');
+// $myGenres = ['avventura', 'azione', 'drammatico'];
+$provaFilm->setGenres(['avventura', 'azione', 'drammatico']);
+var_dump($provaFilm);
+
+var_dump($provaFilm->genres)
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
